d7648e195dcb1649df3231dd09225fa93dbb572a
Osni Pezzini Junior <osni@sotech.xyz>
2022-11-12 00:10:23 -0300

    Revert "fix portable name"
    This reverts commit 0751451ec0261f1897473c7c96f3d282ff2e16e6.

--- libs/portable/generate.py
+++ libs/portable/generate.py
@@ -63,7 +63,7 @@ def build_portable(output_folder: str):
     os.chdir(output_folder)
     os.system("cargo build --release")
 
-# Linux: python3 generate.py -f ../sodesk-portable-packer/test -o . -e ./test/main.py
+# Linux: python3 generate.py -f ../rustdesk-portable-packer/test -o . -e ./test/main.py
 # Windows: python3 .\generate.py -f ..\rustdesk\flutter\build\windows\runner\Debug\ -o . -e ..\rustdesk\flutter\build\windows\runner\Debug\rustdesk.exe
 if __name__ == '__main__':
     parser = optparse.OptionParser()
